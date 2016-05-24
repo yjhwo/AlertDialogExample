68b4bc083bcd453ecaff26b6ba5a41ecfed8efe1
kickscar <kickscar@gmail.com>
2016-05-25 06:16:42 +0900

    Initial commit

---
+++ app/src/main/java/com/estsoft/alertdialogexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.estsoft.alertdialogexample;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
